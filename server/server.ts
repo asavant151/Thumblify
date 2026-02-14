1fa6e8915e7d3c0a33eeb750821762f185439966
asavant151 <asavant151@gmail.com>
2026-02-14 13:10:43 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ await connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-gray.vercel.app'],
     credentials: true
 }));
 
