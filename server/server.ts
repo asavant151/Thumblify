da82b19b583a67b45a9c0bc837805bae39df56e7
asavant151 <asavant151@gmail.com>
2026-02-14 13:16:06 +0530

    cookies properties updated for production

--- server/server.ts
+++ server/server.ts
@@ -1,54 +1,68 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import AuthRouter from './routes/AuthRoutes.js';
-import ThumbnailRouter from './routes/ThumbnailRoutes.js';
-import UserRouter from './routes/UserRoutes.js';
-import PaymentRouter from './routes/PaymentRoutes.js';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import AuthRouter from "./routes/AuthRoutes.js";
+import ThumbnailRouter from "./routes/ThumbnailRoutes.js";
+import UserRouter from "./routes/UserRoutes.js";
+import PaymentRouter from "./routes/PaymentRoutes.js";
 
-declare module 'express-session' {
-    interface SessionData {
-        isLoggedIn: boolean;
-        userId: string;
-    }
+declare module "express-session" {
+  interface SessionData {
+    isLoggedIn: boolean;
+    userId: string;
+  }
 }
 
 await connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-gray.vercel.app'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://thumblify-gray.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 7 days
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      path: "/",
+    },
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI as string,
-        collectionName: 'sessions'
-    })
-}));
+      mongoUrl: process.env.MONGODB_URI as string,
+      collectionName: "sessions",
+    }),
+  }),
+);
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
 
-app.use('/api/auth', AuthRouter);
-app.use('/api/thumbnail', ThumbnailRouter);
-app.use('/api/user', UserRouter);
-app.use('/api/payment', PaymentRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/thumbnail", ThumbnailRouter);
+app.use("/api/user", UserRouter);
+app.use("/api/payment", PaymentRouter);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
